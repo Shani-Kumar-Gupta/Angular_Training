7c400c294387bf790f49d94465c779c6bbbf79ae
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-07 18:26:32 +0530

    used component selector as custom tag, as a class, as attribute

--- my-angular-app/src/app/test/test.component.ts
+++ my-angular-app/src/app/test/test.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
+  //selector: 'app-test', // We can use selector in three ways : (1) Custom tag 
+  //selector: '.app-test', //(2) as a class ('.app-test') 
+  selector: '[app-test]', //(3) inclose selector in [app-test]
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
