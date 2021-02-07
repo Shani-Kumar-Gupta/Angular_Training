d963b458cba26dcfdb1a9f9bfe4b01740c9c7389
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-07 18:34:37 +0530

    used templateUrl and styleUrls as inline template and style

--- my-angular-app/src/app/test/test.component.ts
+++ my-angular-app/src/app/test/test.component.ts
@@ -4,8 +4,16 @@ import { Component, OnInit } from '@angular/core';
   //selector: 'app-test', // We can use selector in three ways : (1) Custom tag 
   //selector: '.app-test', //(2) as a class ('.app-test') 
   selector: '[app-test]', //(3) inclose selector in [app-test]
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  //templateUrl: './test.component.html',
+  // use templateUrl as inline template
+  template: '<div>Hello Inline Template</div>',
+  styleUrls: ['./test.component.css'],
+  // use styleUrls as inline style::::::
+  styles: [`
+  div {
+    color: red;
+  }
+  `],
 })
 export class TestComponent implements OnInit {
 
