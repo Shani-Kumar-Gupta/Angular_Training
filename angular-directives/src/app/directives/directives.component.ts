d2d062975c913686829080801545f21792b13fcd
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-10 17:07:23 +0530

    Angular: ngSwitch directive done

--- angular-directives/src/app/directives/directives.component.ts
+++ angular-directives/src/app/directives/directives.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivesComponent implements OnInit {
 
   displayName = false;
+  public color = "red";
   constructor() { }
 
   ngOnInit(): void {
