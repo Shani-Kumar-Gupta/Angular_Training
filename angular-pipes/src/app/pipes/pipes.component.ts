4f3c35e83f864eb8d3e6043414762a8d51a5de50
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-10 18:09:32 +0530

    Angular: Pipes

---
+++ angular-pipes/src/app/pipes/pipes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css']
+})
+export class PipesComponent implements OnInit {
+
+  public name = "John Doe";
+  public message = "Welcome to learn Angular Pipes";
+  public person = {
+    "firstName" : "Smith",
+    "lastName" : "Johnson"
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
