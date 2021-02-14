f5f745e4724ddcf35b5560f7c4928ace0e93b70f
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-14 20:20:38 +0530

    Created project for lifecycle hooks | created lifecycle-hooks component

---
+++ angular-lifecycle-hooks/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LifecycleHooksComponent } from './lifecycle-hooks/lifecycle-hooks.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LifecycleHooksComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
