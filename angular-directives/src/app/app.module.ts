17b3003e9bceb14084a5f07b4c5700f8b1132e84
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-14 19:22:58 +0530

    Created Custom Module and Component | added bootstrap in our project

--- angular-directives/src/app/app.module.ts
+++ angular-directives/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DirectivesComponent } from './directives/directives.component';
+import { DirectivesModuleModule } from './directives-module/directives-module.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectivesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DirectivesModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
