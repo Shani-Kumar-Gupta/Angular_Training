35bd0eb74d824747b5d1ea17393660fd5614a333
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-15 23:33:12 +0530

    done with basic reactive form layout

--- angular-reactive-forms/src/app/app.component.ts
+++ angular-reactive-forms/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-reactive-forms';
+  signupForm: any;
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl(''),
+      email: new FormControl('')
+    });
+  }
+
+  onSubmit() {
+    console.log("form:::",this.signupForm);
+  }
 }
