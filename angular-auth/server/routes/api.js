6329b9eb3ee6c9e5b6761937d01c00711490947e
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-21 19:45:50 +0530

    AngularAuth: Created USER Registration API

--- angular-auth/server/routes/api.js
+++ angular-auth/server/routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router()
+const User = require('../models/user');
 
 const mongoose = require('mongoose');
 const db = "mongodb+srv://shanidb:162020Shani@authdb.ms3od.mongodb.net/angularauthdb?retryWrites=true&w=majority"
@@ -14,6 +15,19 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, err =>
 
 router.get('/', (req, res) => {
     res.send('From API route');
+})
+
+/* USER Registration API*/
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((error, registerUser) => {
+        if(error){
+            console.log(error);
+        } else {
+            res.status(200).send(registerUser);
+        }
+    })
 })
 
 module.exports = router
