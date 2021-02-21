53c6fee817403ea0c2fec783d54a93c842cf325e
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-02-21 19:20:49 +0530

    AngularAuth: Connected to MongoDB

--- angular-auth/server/routes/api.js
+++ angular-auth/server/routes/api.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router()
 
+const mongoose = require('mongoose');
+const db = "mongodb+srv://shanidb:162020Shani@authdb.ms3od.mongodb.net/angularauthdb?retryWrites=true&w=majority"
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+    if(err){
+        console.log('Error!' + err)
+    } else {
+        console.log('Connected to mongodb');
+    }
+})
+
 router.get('/', (req, res) => {
     res.send('From API route');
 })
