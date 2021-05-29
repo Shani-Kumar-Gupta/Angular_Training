210ef6f903041d4b97e7b05907fd70d49763f29b
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-05-29 23:10:27 +0530

    changed title

--- angular10days/src/app/app.component.ts
+++ angular10days/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ // decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular10days';
+  title = 'Welcome To Learn Angular in 10 Days';
 }
