985123f07fc493cc650c4d279ad31fb32e6e670e
Shani-Kumar-Gupta <shani.gupta@geminisolutions.com>
2022-05-06 17:24:18 +0530

    services

---
+++ index.js
@@ -0,0 +1,9 @@
+let myName = {
+  name: "Shani"
+};
+
+function greet(name) {
+  console.log(name);
+}
+
+greet(myName);
---
+++ myfile.ts
@@ -0,0 +1,11 @@
+function greet1(name: string) {
+  console.log(name);
+}
+
+let firstName: string;
+let num: number = 20;
+
+firstName = '';
+
+greet1(firstName);
+//greet1(num);
